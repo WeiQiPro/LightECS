536f142331d2db14b9865a0cde76f293e52294e7
Jeremiah Donley <108106416+WeiQiPro@users.noreply.github.com>
2023-10-31 09:12:03 -0400

    new Class Canvas
    Allows developer a basic shapes and image draw functions.
    In addition you can add functions and properties as you would like.

--- LightECS.js
+++ LightECS.js
@@ -73,6 +73,148 @@ class System {
     }
 }
 
+class Canvas {
+    static DEFAULT_WIDTH = 1280;
+    static DEFAULT_HEIGHT = 720;
+
+    constructor(config = {}) {
+        this.canvas = this.createCanvas(config.width, config.height);
+        this.context = this.canvas.getContext('2d');
+    }
+
+    addFunction(method) {
+        if (typeof method === 'function' && method.name !== '') {
+            this[method.name] = method.bind(this);
+        }
+    }
+
+    addProperty(property){
+        if(typeof property === 'function') return console.log('Error: adding function to properties');
+        
+        Object.assign(this, property)
+    }
+
+    createCanvas(width = Graphic2d.DEFAULT_WIDTH, height = Graphic2d.DEFAULT_HEIGHT) {
+        const canvas = document.createElement('canvas');
+        canvas.width = width;
+        canvas.height = height;
+        document.body.appendChild(canvas); // Optionally attach it to the body or any other container
+        return canvas;
+    }
+
+    clearCanvas() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    arc(x, y, radius, startAngle, endAngle, fillColor = null, strokeColor = 'black', strokeWidth = 1) {
+        this.context.beginPath();
+        this.context.arc(x, y, radius, startAngle, endAngle);
+        this.context.closePath();
+    
+        if (fillColor) {
+            this.context.fillStyle = fillColor;
+            this.context.fill();
+        }
+        if (strokeColor) {
+            this.context.strokeStyle = strokeColor;
+            this.context.lineWidth = strokeWidth;
+            this.context.stroke();
+        }
+    }
+  
+    circle(x, y, radius, fillColor = null, strokeColor = 'black', strokeWidth = 1) {
+        this.arc(x, y, radius, 0, 2 * Math.PI, fillColor, strokeColor, strokeWidth);
+    }
+
+    ellipse(x, y, radiusX, radiusY, rotation = 0, startAngle = 0, endAngle = 2 * Math.PI, fillColor = null, strokeColor = 'black', strokeWidth = 1) {
+        this.context.beginPath();
+        this.context.ellipse(x, y, radiusX, radiusY, rotation, startAngle, endAngle);
+        this.context.closePath();
+    
+        if (fillColor) {
+            this.context.fillStyle = fillColor;
+            this.context.fill();
+        }
+        if (strokeColor) {
+            this.context.strokeStyle = strokeColor;
+            this.context.lineWidth = strokeWidth;
+            this.context.stroke();
+        }
+    }
+    
+    image(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight) {
+        this.context.drawImage(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+    }
+
+    line(x1, y1, x2, y2, color = 'black', strokeWidth = 1) {
+        this.context.beginPath();
+        this.context.moveTo(x1, y1);
+        this.context.lineTo(x2, y2);
+        this.context.strokeStyle = color;
+        this.context.lineWidth = strokeWidth;
+        this.context.stroke();
+    }
+    
+    point(x, y, color = 'black') {
+        this.context.fillStyle = color;
+        this.context.fillRect(x, y, 1, 1);
+    }
+    
+    quad(x1, y1, x2, y2, x3, y3, x4, y4, fillColor = null, strokeColor = 'black', strokeWidth = 1) {
+        this.context.beginPath();
+        this.context.moveTo(x1, y1);
+        this.context.lineTo(x2, y2);
+        this.context.lineTo(x3, y3);
+        this.context.lineTo(x4, y4);
+        this.context.closePath();
+        
+        if (fillColor) {
+            this.context.fillStyle = fillColor;
+            this.context.fill();
+        }
+        if (strokeColor) {
+            this.context.strokeStyle = strokeColor;
+            this.context.lineWidth = strokeWidth;
+            this.context.stroke();
+        }
+    }
+
+    rect(x, y, width, height, fillColor = null, strokeColor = 'black', strokeWidth = 1) {
+        if (fillColor) {
+            this.context.fillStyle = fillColor;
+            this.context.fillRect(x, y, width, height);
+        }
+        if (strokeColor) {
+            this.context.strokeStyle = strokeColor;
+            this.context.lineWidth = strokeWidth;
+            this.context.strokeRect(x, y, width, height);
+        }
+    }
+
+    square(x, y, sideLength, fillColor = null, strokeColor = 'black', strokeWidth = 1) {
+        this.rect(x, y, sideLength, sideLength, fillColor, strokeColor, strokeWidth);
+    }
+
+    triangle(x1, y1, x2, y2, x3, y3, fillColor = null, strokeColor = 'black', strokeWidth = 1) {
+        this.context.beginPath();
+        this.context.moveTo(x1, y1);
+        this.context.lineTo(x2, y2);
+        this.context.lineTo(x3, y3);
+        this.context.closePath();
+    
+        if (fillColor) {
+            this.context.fillStyle = fillColor;
+            this.context.fill();
+        }
+        if (strokeColor) {
+            this.context.strokeStyle = strokeColor;
+            this.context.lineWidth = strokeWidth;
+            this.context.stroke();
+        }
+    }
+
+}
+
 function ULECSID() {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     
@@ -99,3 +241,5 @@ function ULECSID() {
 
     return firstUniqueString + "-" + secondUniqueString.match(/.{5}/g).join('-')
 }
+
+module.export = { Entity, Component, System, ULECSID }
